0872ad1401e1f33c774244dbbcccb8fa207ec0e8
Jeffrey N. Johnson <jeff@cohere-llc.com>
2020-09-11 17:39:02 -0700

    Revamped YamlFile class.

--- driver/yaml_file.cpp
+++ driver/yaml_file.cpp
@@ -2,7 +2,7 @@
 
 namespace haero {
 
-Yaml_file::Yaml_file(const std::string& filename)
+YamlFile::YamlFile(const std::string& filename)
 {
   // Try to load the thing.
   try
@@ -11,55 +11,91 @@ Yaml_file::Yaml_file(const std::string& filename)
   }
   catch (YAML::BadFile& e)
   {
-    throw Yaml_exception(e.what());
+    throw YamlException(e.what());
   }
   catch (YAML::ParserException& e)
   {
-    throw Yaml_exception(e.what());
+    throw YamlException(e.what());
   }
 }
 
-Yaml_file::~Yaml_file() {
+YamlFile::~YamlFile() {
 }
 
-std::vector<Mode> Yaml_file::read_modes() const
+std::vector<Mode> YamlFile::read_modes() const
 {
   std::vector<Mode> modes;
-  if (_root["modes"] and _root["modes"].IsSequence())
+  if (_root["modes"] and _root["modes"].IsMap())
   {
-    YAML::Node node = _root["modes"];
-    for (size_t i = 0; i < node.size(); ++i)
+    auto node = _root["modes"];
+    for (auto iter = node.begin(); iter != node.end(); ++iter)
     {
-      if (not node[i]["name"])
-        throw Yaml_exception("mode entry has no name.");
-      else if (not node[i]["D_min"])
-        throw Yaml_exception("mode entry has no minimum diameter (D_min).");
-      else if (not node[i]["D_max"])
-        throw Yaml_exception("mode entry has no maximum diameter (D_max).");
-      else if (not node[i]["sigma"])
-        throw Yaml_exception("mode entry has no geometric stddev (sigma).");
+      std::string name = iter->first.as<std::string>();
+      auto mnode = iter->second;
+      if (not mnode["D_min"])
+        throw YamlException("mode entry has no minimum diameter (D_min).");
+      else if (not mnode["D_max"])
+        throw YamlException("mode entry has no maximum diameter (D_max).");
+      else if (not mnode["sigma"])
+        throw YamlException("mode entry has no geometric stddev (sigma).");
       else
       {
-        modes.push_back(Mode(node[i]["name"].as<std::string>(),
-                             node[i]["D_min"].as<Real>(),
-                             node[i]["D_max"].as<Real>(),
-                             node[i]["sigma"].as<Real>()));
+        modes.push_back(Mode(name,
+                             mnode["D_min"].as<Real>(),
+                             mnode["D_max"].as<Real>(),
+                             mnode["sigma"].as<Real>()));
       }
     }
   }
   return modes;
 }
 
-std::vector<Species> Yaml_file::read_species() const
+std::vector<Species> YamlFile::read_aerosol_species() const
 {
   std::vector<Species> species;
+  if (_root["aerosols"] and _root["aerosols"].IsMap())
+  {
+    auto node = _root["aerosols"];
+    for (auto iter = node.begin(); iter != node.end(); ++iter)
+    {
+      std::string symbol = iter->first.as<std::string>();
+      auto snode = iter->second;
+      if (not snode["name"])
+        throw YamlException("aerosol species entry has no name.");
+      else
+      {
+        Species s;
+        s.symbol = symbol;
+        s.name = snode["name"].as<std::string>();
+        species.push_back(s);
+      }
+    }
+  }
   return species;
 }
 
-std::vector<Reaction> Yaml_file::read_reactions() const
+std::vector<Species> YamlFile::read_gas_species() const
 {
-  std::vector<Reaction> reactions;
-  return reactions;
+  std::vector<Species> species;
+  if (_root["gases"] and _root["gases"].IsMap())
+  {
+    auto node = _root["gases"];
+    for (auto iter = node.begin(); iter != node.end(); ++iter)
+    {
+      std::string symbol = iter->first.as<std::string>();
+      auto snode = iter->second;
+      if (not snode["name"])
+        throw YamlException("gas species entry has no name.");
+      else
+      {
+        Species s;
+        s.symbol = symbol;
+        s.name = snode["name"].as<std::string>();
+        species.push_back(s);
+      }
+    }
+  }
+  return species;
 }
 
 namespace {
@@ -112,86 +148,43 @@ read_vector_from_map(const YAML::Node& node, const std::string& map_name)
 
 } // anonymous namespace
 
-std::vector<std::map<std::string, Real> > Yaml_file::read_initial_conditions() const
+std::vector<std::map<std::string, Real> > YamlFile::read_initial_conditions() const
 {
   return read_vector_from_map(_root, "initial_conditions");
 }
 
-Real Yaml_file::read_perturb_factor() const
+std::map<std::string, bool> YamlFile::read_physics_settings() const
 {
-  Real pert_factor = 0.0;
-  if (_root["perturbation"])
-    pert_factor = _root["perturbation"].as<Real>();
-  return pert_factor;
+  std::map<std::string, bool> settings;
+  if (_root["physics"] and _root["physics"].IsMap())
+  {
+    auto node = _root["physics"];
+    for (auto iter = node.begin(); iter != node.end(); ++iter)
+      settings[iter->first.as<std::string>()] = iter->second.as<bool>();
+  }
+  return settings;
 }
 
-void Yaml_file::read_control_params(int& do_gaschem, int& do_cloudchem,
-                                    int& do_gasaerexch, int& do_rename,
-                                    int& do_newnuc, int& do_coag,
-                                    int& do_calcsize, int& num_unit,
-                                    int& frac_unit, int& gas_unit)
+std::pair<std::string, std::map<std::string, Real> > YamlFile::read_atmosphere() const
 {
-  do_gaschem = 0;
-  do_cloudchem = 0;
-  do_gasaerexch = 0;
-  do_rename = 0;
-  do_newnuc = 0;
-  do_coag = 0;
-  do_calcsize = 0;
-  num_unit = 0;
-  frac_unit = 0;
-  gas_unit = 0;
-  if (_root["parameters"] and _root["parameters"].IsMap())
+  std::pair<std::string, std::map<std::string, Real> > atm;
+  if (_root["atmosphere"] and _root["atmosphere"].IsMap())
   {
-    auto node = _root["parameters"];
-    if (node["do_gaschem"])
-      do_gaschem = node["do_gaschem"].as<int>();
-    if (node["do_cloudchem"])
-      do_cloudchem = node["do_cloudchem"].as<int>();
-    if (node["do_gasaerexch"])
-      do_gasaerexch = node["do_gasaerexch"].as<int>();
-    if (node["do_rename"])
-      do_rename = node["do_rename"].as<int>();
-    if (node["do_newnuc"])
-      do_newnuc = node["do_newnuc"].as<int>();
-    if (node["do_coag"])
-      do_coag = node["do_coag"].as<int>();
-    if (node["do_calcsize"])
-      do_coag = node["do_calcsize"].as<int>();
-    if (node["num_unit"])
-      num_unit = node["num_unit"].as<int>();
-    if (node["frac_unit"])
-      frac_unit = node["frac_unit"].as<int>();
-    if (node["gas_unit"])
-      gas_unit = node["gas_unit"].as<int>();
+    auto node = _root["atmosphere"];
+    if (node["model"])
+      atm.first = node["model"].as<std::string>();
+    else
+      throw YamlException("No model found in atmosphere section.");
+    for (auto iter = node.begin(); iter != node.end(); ++iter)
+    {
+      auto key = iter->first.as<std::string>();
+      if (key != "model")
+        atm.second[key] = iter->second.as<Real>();
+    }
   }
 }
 
-void Yaml_file::read_met_input(Real& temp, Real& press, Real& RH_CLEA,
-                               Real& hgt, Real& cld_frac)
-{
-  temp = 300.0;
-  press = 3.0e4;
-  RH_CLEA = 0.5;
-  hgt = 7500.0;
-  cld_frac = 0.0;
-  if (_root["met_input"] and _root["met_input"].IsMap())
-  {
-    auto node = _root["met_input"];
-    if (node["temp"])
-      temp = node["temp"].as<Real>();
-    if (node["press"])
-      press = node["press"].as<Real>();
-    if (node["RH_CLEA"])
-      RH_CLEA = node["RH_CLEA"].as<Real>();
-    if (node["hgt"])
-      hgt = node["hgt"].as<Real>();
-    if (node["cld_frac"])
-      cld_frac = node["cld_frac"].as<Real>();
-  }
-}
-
-std::vector<Real> Yaml_file::read_timesteps() const
+std::vector<Real> YamlFile::read_timesteps() const
 {
   std::vector<Real> dts;
   if (_root["timestep"])
@@ -208,23 +201,18 @@ std::vector<Real> Yaml_file::read_timesteps() const
   return dts;
 }
 
-Real Yaml_file::read_duration() const
+Real YamlFile::read_duration() const
 {
   if (not _root["duration"])
-    throw Yaml_exception("No duration found in YAML file.");
+    throw YamlException("No duration found in YAML file.");
   else
-  {
     return _root["duration"].as<Real>();
-  }
 }
 
-void Yaml_file::read_output_params(std::string& dir,
-                                   std::string& prefix,
-                                   int& freq) const
+std::pair<std::string, std::string> YamlFile::read_output_params() const
 {
-  prefix = std::string("mam_output");
-  dir = std::string(".");
-  freq = -1;
+  auto prefix = std::string("mam_output");
+  auto dir = std::string(".");
   if (_root["output"])
   {
     auto node = _root["output"];
@@ -232,9 +220,20 @@ void Yaml_file::read_output_params(std::string& dir,
       prefix = node["prefix"].as<std::string>();
     if (node["directory"])
       dir = node["directory"].as<std::string>();
+  }
+  return std::make_pair(prefix, dir);
+}
+
+int YamlFile::read_output_freq() const
+{
+  int freq = -1;
+  if (_root["output"])
+  {
+    auto node = _root["output"];
     if (node["frequency"])
       freq = node["frequency"].as<int>();
   }
+  return freq;
 }
 
 } // namespace mam
--- driver/yaml_file.hpp
+++ driver/yaml_file.hpp
@@ -1,22 +1,21 @@
-#ifndef HAERO_YAML_FILE_HPP
-#define HAERO_YAML_FILE_HPP
+#ifndef HAERO_YAMLFILE_HPP
+#define HAERO_YAMLFILE_HPP
 
 #include <vector>
 #include <map>
 #include <yaml-cpp/yaml.h>
 #include "haero/mode.hpp"
 #include "haero/species.hpp"
-#include "haero/reaction.hpp"
 
 namespace haero {
 
 /// This exception class stores information about errors encountered in reading
 /// data from a YAML file.
-class Yaml_exception: public std::exception {
+class YamlException: public std::exception {
   public:
 
   /// Constructs an exception containing the given descriptive message.
-  Yaml_exception(const std::string& message):
+  YamlException(const std::string& message):
     _message(message) {}
 
   const char* what() const throw() {
@@ -32,69 +31,45 @@ class Yaml_exception: public std::exception {
 /// interface supports an "ensemble mode" in which the parameters for several
 /// related simulations may be defined succinctly within a single input file.
 /// Each of the methods on this interface may throw a Yaml_exception.
-class Yaml_file {
+class YamlFile {
   public:
 
   /// Given a filename, creates a YAML file from input can be read.
   /// @param [in] filename An absolute path to a YAML file to be read.
-  Yaml_file(const std::string& filename);
+  YamlFile(const std::string& filename);
 
   // Disallowed machinery.
-  Yaml_file(const Yaml_file&) = delete;
-  Yaml_file& operator=(const Yaml_file&) = delete;
+  YamlFile(const YamlFile&) = delete;
+  YamlFile& operator=(const YamlFile&) = delete;
 
   /// Destructor.
-  ~Yaml_file();
+  ~YamlFile();
 
   /// Reads data for all modes in this input file into a vector.
   /// @returns the vector of modes read from the file.
   std::vector<Mode> read_modes() const;
 
-  /// Reads data for all species in this input file into a vector.
+  /// Reads data for all aerosol species in this input file into a vector.
   /// @returns the vector of species read from the file.
-  std::vector<Species> read_species() const;
+  std::vector<Species> read_aerosol_species() const;
 
-  /// Reads data for all chemical reactions in this input file into a vector.
-  /// @returns the vector of reactions read from the file.
-  std::vector<Reaction> read_reactions() const;
+  /// Reads data for all gas species in this input file into a vector.
+  /// @returns the vector of species read from the file.
+  std::vector<Species> read_gas_species() const;
+
+  /// Reads data for activating/deactivating various physical processes.
+  /// @returns a map from process names to booleans (on/off settings).
+  std::map<std::string, bool> read_physics_settings() const;
+
+  /// Reads data for ambient atmospheric conditions. Returns the name of
+  /// the model and a mapping of parameter names to their values.
+  std::pair<std::string, std::map<std::string, Real> > read_atmosphere() const;
 
   /// Reads data for all initial conditions in this input file into a vector. A
   /// set of initial conditions is a mapping of prognostic variable names onto
   /// real-valued initial quantities.
   /// @returns the vector of initial conditions read from the file.
   std::vector<std::map<std::string, Real> > read_initial_conditions() const;
-
-  /// Reads the constant perturbation factor to be applied to all initial
-  /// conditions in this input file (across all timesteps and members of
-  /// ensembles). This factor is multipled by machine epsilon and added to
-  /// each initial condition.
-  /// @returns the perturbaton factor for initial conditions.
-  Real read_perturb_factor() const;
-
-  /// Reads simulation control parameters.
-  /// \param [out] do_gaschem 1 if gas chemistry enabled, 0 otherwise
-  /// \param [out] do_cloudchem 1 if cloud chemistry enabled, 0 otherwise
-  /// \param [out] do_gasaerexch 1 if gas-aerosol exchange enabled, 0 otherwise
-  /// \param [out] do_rename 1 if ??? enabled, 0 otherwise
-  /// \param [out] do_newnuc 1 if nucleation(?) enabled, 0 otherwise
-  /// \param [out] do_coag 1 if coagulation enabled, 0 otherwise
-  /// \param [out] do_calcsize 1 if ??? enabled, 0 otherwise
-  /// \param [out] num_unit ???
-  /// \param [out] frac_unit ???
-  /// \param [out] gas_unit ???
-  void read_control_params(int& do_gaschem, int& do_cloudchem,
-                           int& do_gasaerexch, int& do_rename, int& do_newnuc,
-                           int& do_coag, int& do_calcsize, int& num_unit,
-                           int& frac_unit, int& gas_unit);
-
-  /// Reads "MET" (background?) atmospheric parameters.
-  /// \param [out] temp air temperature [K]
-  /// \param [out] press pressure [Pa]
-  /// \param [out] RH_CLEA clear-sky RH [?]
-  /// \param [out] hgt height above Earth's surface [m]
-  /// \param [out] cld_frac cloud fraction [-]
-  void read_met_input(Real& temp, Real& press, Real& RH_CLEA, Real& hgt,
-                      Real& cld_frac);
 
   /// Reads data for all timestep sizes for simulations into a vector.
   /// @returns the vector of timestep sizes read from the file.
@@ -103,18 +78,12 @@ class Yaml_file {
   /// Reads the duration for all simulations within this file.
   Real read_duration() const;
 
-  /// Reads output parameters.
-  /// \param [out] dir the directory to which haero output files are written
-  ///                  (default: ".")
-  /// \param [out] prefix the prefix for haero output files (default: "mam_output")
-  /// \param [out] freq the frequency at which output is recorded, expressed as
-  ///                   the number of steps between recordings.
-  void read_output_params(std::string& dir,
-                          std::string& prefix,
-                          int& freq) const;
+  /// Reads the directory and prefix for output files.
+  std::pair<std::string, std::string> read_output_params() const;
 
-  /// Reads the directory in which haero output files are written.
-  std::string read_output_dir() const;
+  /// Reads the output frequency (in steps). Returns -1 if no frequency is
+  /// given.
+  int read_output_freq() const;
 
   private:
 
