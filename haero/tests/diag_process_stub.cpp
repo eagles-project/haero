1808caf138a3c54211287a7feb2f6351e6d83d0b
Jeffrey N. Johnson <jeff@cohere-llc.com>
2020-12-16 17:06:03 -0800

    Put together a simple ideal gas law diagnostic process.

--- haero/tests/diag_process_stub.cpp
+++ haero/tests/diag_process_stub.cpp
@@ -16,7 +16,7 @@ namespace haero {
 
 DiagProcessStub::DiagProcessStub():
   FDiagnosticProcess(haero::WaterUptakeProcess, "Diagnostic process stub (Fortran)",
-                     {}, {}, {}, {"pressure"},
+                     {"temperature"}, {}, {"pressure"}, {"pressure"},
                      diag_stub_init, diag_stub_update, diag_stub_finalize) {
 }
 
