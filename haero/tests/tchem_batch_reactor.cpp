287799ed9906cf0f1268bfa17bada8d01321620b
Jeffrey N. Johnson <jeff@cohere-llc.com>
2021-02-10 17:00:49 -0800

    Made batch reactor test self-contained.

--- haero/tests/tchem_batch_reactor.cpp
+++ haero/tests/tchem_batch_reactor.cpp
@@ -3,11 +3,46 @@
 #include "catch2/catch.hpp"
 #include "haero/floating_point.hpp"
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
 using namespace haero;
 using namespace chemUtil;
 
 TEST_CASE("toy problem chemistry", "haero_unit_tests")
 {
+  // Write out some test data to our current working directory.
+  const char* chem_inp = R"INPUT(ELEMENTS
+X /1/
+END
+SPECIES
+X X2
+END
+THERM ALL
+    300.000  1000.000  5000.000
+X                        X  1               G   200.000  6000.000 1000.00      1
+ 2.50000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00    2
+-7.45375000E+02 4.37967000E+00 2.50000000E+00 0.00000000E+00 0.00000000E+00    3
+ 0.00000000E+00 0.00000000E+00-7.45375000E+02 4.37967000E+00                   4
+X2                       X  2               G   200.000  6000.000 1000.00      1
+ 2.50000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00 0.00000000E+00    2
+-7.45375000E+02 4.37967000E+00 2.50000000E+00 0.00000000E+00 0.00000000E+00    3
+ 0.00000000E+00 0.00000000E+00-7.45375000E+02 4.37967000E+00                   4
+END
+REACTIONS
+X2=>2X      10E+0    1   1
+X+X=>X2      10E+0    1   1
+END
+)INPUT";
+  mkdir("data", 0777);
+  mkdir("data/toy-problem", 0777);
+  FILE* f = fopen("data/toy-problem/chem.inp", "w");
+  fprintf(f, "%s", chem_inp);
+  fclose(f);
+  f = fopen("data/toy-problem/therm.dat", "w");
+  fclose(f);
+
+  // Run the test.
   Kokkos::initialize();
   {
 
