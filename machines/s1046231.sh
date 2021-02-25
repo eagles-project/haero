59582ced32810087f73ef69f25e88b2a11fc46a3
Michael J. Schmidt <mschmidt271@gmail.com>
2021-02-25 12:28:42 -0700

    fixes build for pre-built tines

--- machines/s1046231.sh
+++ machines/s1046231.sh
@@ -20,7 +20,7 @@ export OPENBLAS_INCLUDE_DIR=/usr/local/opt/openblas/include
 export OPENBLAS_LIBRARY_DIR=/usr/local/opt/openblas/lib
 export OPENBLAS_LIBRARY=libopenblas.a
 
-export TINES_INCLUDE_DIR=$HOME/Tines/build/install/include
+export TINES_INCLUDE_DIR=$HOME/Tines/build/install/include/tines
 export TINES_LIBRARY_DIR=$HOME/Tines/build/install/lib
 export TINES_LIBRARY=libtines.a
 
