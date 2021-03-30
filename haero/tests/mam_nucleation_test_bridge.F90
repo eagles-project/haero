f03c5ee1a4c2a3624acc27bcf4297c4e3776de68
Overfelt <jroverf@sandia.gov>
2021-03-30 13:08:40 -0600

    Continue implementation of C++ version of MAM Nucleation F90 code.
    Start adding the F90 -> C++ functions one by one with unit testing
    for both CPU and GPU.  Add pbl_nuc_wang2008 to mam_nucleation_process
    and unit tests.

--- haero/tests/mam_nucleation_test_bridge.F90
+++ haero/tests/mam_nucleation_test_bridge.F90
@@ -7,11 +7,11 @@ module mam_nucleation_test_bridge
 
   ! Module functions
   public :: ternary_nuc_merik2007_bridge
+  public :: binary_nuc_vehk2002_bridge
 
 contains
 
 subroutine ternary_nuc_merik2007_bridge(t, rh, c2, c3, j_log, ntot, nacid, namm, r) bind(c)
-  use iso_c_binding, only: c_ptr
   use haero, only: wp
   use mam_nucleation, only: ternary_nuc_merik2007
   implicit none
@@ -22,6 +22,41 @@ subroutine ternary_nuc_merik2007_bridge(t, rh, c2, c3, j_log, ntot, nacid, namm,
 
   ! Call the actual subroutine.
   call ternary_nuc_merik2007(t, rh, c2, c3, j_log, ntot, nacid, namm, r)
+end subroutine
+
+subroutine binary_nuc_vehk2002_bridge(temp, rh, so4vol, ratenucl, rateloge, cnum_h2so4, cnum_tot, radius_cluster) bind(c)
+  use haero, only: wp
+  use mam_nucleation, only: binary_nuc_vehk2002
+  implicit none
+
+  ! Arguments
+  real(wp), value, intent(in) :: temp, rh, so4vol
+  real(wp), intent(out) :: ratenucl, rateloge, cnum_h2so4, cnum_tot, radius_cluster
+
+  ! Call the actual subroutine.
+  call binary_nuc_vehk2002(temp, rh, so4vol, ratenucl, rateloge, cnum_h2so4, cnum_tot, radius_cluster)
+end subroutine
+
+subroutine pbl_nuc_wang2008_bridge(factor_pbl_ratenucl, so4vol, flagaa, flagaa2, ratenucl, rateloge, &
+  cnum_tot, cnum_h2so4, cnum_nh3, radius_cluster) bind(c)
+  use iso_c_binding, only: c_int
+  use haero, only: wp
+  use mam_nucleation, only: pbl_nuc_wang2008
+  use mam_nucleation, only: adjust_factor_pbl_ratenucl
+  implicit none
+
+  ! Arguments
+  real(wp), value, intent(in) :: factor_pbl_ratenucl
+  real(wp), value, intent(in) :: so4vol
+  integer(c_int), value, intent(in) :: flagaa
+  integer(c_int), intent(inout) :: flagaa2
+  real(wp), intent(inout) :: ratenucl, rateloge, cnum_tot, cnum_h2so4, cnum_nh3, radius_cluster
+
+  ! Call the actual subroutine.
+  ! But first set this public value on the module that the function will use.
+  adjust_factor_pbl_ratenucl = factor_pbl_ratenucl
+  call pbl_nuc_wang2008(so4vol, flagaa, flagaa2, ratenucl, rateloge, &
+    cnum_tot, cnum_h2so4, cnum_nh3, radius_cluster)
 end subroutine
 
 end module
