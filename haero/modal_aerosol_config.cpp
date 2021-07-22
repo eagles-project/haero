973c5552e1f76864f95f1708529e0121f8bd7dc7
Jeffrey N. Johnson <jeff@cohere-llc.com>
2021-07-21 17:00:28 -0700

    Stripping out device stuff from modal aerosol config.

--- haero/modal_aerosol_config.cpp
+++ haero/modal_aerosol_config.cpp
@@ -9,50 +9,39 @@ namespace haero {
 
 // This sets mode->species indexing. Throws an exception if the mode_species
 // mapping produces an inconsistent configuration.
-void ModalAerosolConfig::index_modal_species(
+void ModalAerosolConfig::index_mode_species(
     const std::map<std::string, std::vector<std::string>>& mode_species) {
-  HostType::view_1d<const Mode> aerosol_modes = h_aerosol_modes;
-  std::vector<std::vector<int>> species_for_mode(aerosol_modes.size());
+  species_for_mode_.clear();
+  for (auto kv_pair: mode_species) {
+    const std::string& mode_name = kv_pair.first;
+    const std::vector<std::string>& aero_species = kv_pair.second;
 
-  h_n_species_per_mode = Kokkos::create_mirror_view(d_n_species_per_mode);
+    int mode_index = 0;
+    while (mode_index < aerosol_modes.size()) {
+      if (aerosol_modes[mode_index].name() == mode_name) break;
+      mode_index++;
+    }
 
-  for (auto iter = mode_species.begin(); iter != mode_species.end(); ++iter) {
-    const std::string& mode_name = iter->first;
-    const std::vector<std::string>& aero_species = iter->second;
+    if (mode_index == aerosol_modes.size()) continue;
 
-    int mode_index = -1;
-    for (unsigned i = 0; i < aerosol_modes.size() && mode_index == -1; ++i)
-      if (aerosol_modes[i].name() == mode_name) mode_index = i;
-
-    for (int s = 0; s < aero_species.size(); ++s) {
-      int species_index = -1;
-      for (unsigned i = 0; i < h_aerosol_species.size() && species_index == -1;
-           ++i)
-        if (h_aerosol_species[i].symbol() == aero_species[s]) species_index = i;
-      species_for_mode[mode_index].push_back(species_index);
+    for (const auto& species_name: aero_species) {
+      int species_index = 0;
+      while (species_index < aero_species.size()) {
+        if (aerosol_species[species_index].symbol() == species_name) break;
+        species_index++;
+      }
+      if (species_index < aerosol_species.size()) {
+        species_for_mode_[mode_index].push_back(species_index);
+      }
     }
   }
 
   // Make sure each mode contains at least one species.
-  for (int m = 0; m < species_for_mode.size(); ++m) {
-    EKAT_REQUIRE_MSG(not species_for_mode[m].empty(),
+  for (int m = 0; m < species_for_mode_.size(); ++m) {
+    EKAT_REQUIRE_MSG(not species_for_mode_[m].empty(),
                      aerosol_modes[m].name().c_str()
                          << " mode contains no aerosol species!");
-    h_n_species_per_mode(m) = species_for_mode[m].size();
   }
-  Kokkos::deep_copy(d_n_species_per_mode, h_n_species_per_mode);
-
-  // Move to a 2D View by making rectangular and filling ragged arrays with -1.
-  unsigned long max_num_species = 0;
-  for (int m = 0; m < species_for_mode.size(); ++m)
-    max_num_species = std::max(max_num_species, species_for_mode[m].size());
-  for (int m = 0; m < species_for_mode.size(); ++m)
-    for (int s = species_for_mode[m].size(); s < max_num_species; ++s)
-      species_for_mode[m].push_back(-1);
-  d_species_for_mode = vector_to_2dview(species_for_mode, "species_for_mode");
-
-  h_species_for_mode = Kokkos::create_mirror_view(d_species_for_mode);
-  Kokkos::deep_copy(h_species_for_mode, d_species_for_mode);
 }
 
 std::string ModalAerosolConfig::info_string(const int tab_level) const {
@@ -65,12 +54,12 @@ std::string ModalAerosolConfig::info_string(const int tab_level) const {
      << "species"
      << "\n";
   for (int m = 0; m < num_modes(); ++m) {
-    ss << std::setw(20) << h_aerosol_modes(m).name() << std::setw(7)
-       << h_n_species_per_mode(m) << "  ";
+    ss << std::setw(20) << aerosol_modes[m].name() << std::setw(7)
+       << species_for_mode_[m].size() << "  ";
     const auto species_for_mode = aerosol_species_for_mode(m);
-    for (int s = 0; s < h_n_species_per_mode(m); ++s) {
+    for (int s = 0; s < species_for_mode.size(); ++s) {
       ss << species_for_mode[s].symbol()
-         << (s < h_n_species_per_mode(m) - 1 ? ", " : "\n");
+         << (s < species_for_mode.size() - 1 ? ", " : "\n");
     }
     ss << "\n";
   }
