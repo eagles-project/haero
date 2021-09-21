a4630540062702f97f54572174d83aec08da112d
Asher Mancinelli <ashermancinelli@gmail.com>
2021-09-21 17:00:24 -0600

    Use views for data over vectors

--- haero/processes/mam_rename_process.hpp
+++ haero/processes/mam_rename_process.hpp
@@ -6,16 +6,25 @@
 #include "kokkos/Kokkos_Core.hpp"
 #include "kokkos/Kokkos_Vector.hpp"
 
+namespace {
+
+// KOKKOS_INLINE_FUNCTION
+// void compute_dryvol_change_in_src_mode(int num_modes, int dest_mode_of_mode,
+//     bool is_cloudy, qi_vmr, qi_del_growth, qcld_vmr, qcld_del_growth,
+//     dryvol_a, deldryvol_a, dryvol_c, deldryvol_c) {
+// 
+// }
+
+}
+
 namespace haero {
 
 /// \brief Bindings for the rename subroutine
 class MAMRenameProcess final : public DeviceAerosolProcess<MAMRenameProcess> {
  public:
-  template <typename T>
-  using Container = Kokkos::vector<T>;
 
-  using Integral = int;
-  using Size = std::size_t;
+  using integral_type = int;
+  using size_type = std::size_t;
 
   MAMRenameProcess();
 
@@ -33,25 +42,38 @@ class MAMRenameProcess final : public DeviceAerosolProcess<MAMRenameProcess> {
   void run_(const TeamType& team, Real t, Real dt,
             const Prognostics& prognostics, const Atmosphere& atmosphere,
             const Diagnostics& diagnostics,
-            const Tendencies& tendencies) const override {}
+            const Tendencies& tendencies) const override {
+    // compute_dryvol_change_in_src_mode(
+    //     num_modes, dest_mode_of_mode, iscldy, qi_vmr, qi_del_growth, qcld_vmr,
+    //     qcld_del_growth, dryvol_a, deldryvol_a, dryvol_c, deldryvol_c);
+  }
 
  private:
-  void find_renaming_pairs_(
-      const ModalAerosolConfig& config,
-      const Container<Integral>& dest_mode_of_mode_mapping, Size& num_pairs,
-      Container<Real>& sz_factor, Container<Real>& fmode_dist_tail_fac,
-      Container<Real>& v2n_lo_rlx, Container<Real>& v2n_hi_rlx,
-      Container<Real>& ln_diameter_tail_fac, Container<Real>& diameter_cutoff,
-      Container<Real>& ln_dia_cutoff, Container<Real>& diameter_belowcutoff,
-      Container<Real>& dryvol_smallest) const;
+  void find_renaming_pairs_(const ModalAerosolConfig& config,
+                            view_1d_int_type& dest_mode_of_mode_mapping,
+                            size_type& num_pairs, view_1d_pack_type& size_factor,
+                            view_1d_pack_type& fmode_dist_tail_fac,
+                            view_1d_pack_type& volume2num_lo_relaxed,
+                            view_1d_pack_type& volume2num_hi_relaxed,
+                            view_1d_pack_type& ln_diameter_tail_fac,
+                            view_1d_pack_type& diameter_cutoff,
+                            view_1d_pack_type& ln_dia_cutoff,
+                            view_1d_pack_type& diameter_belowcutoff,
+                            view_1d_pack_type& dryvol_smallest) const;
 
  private:
   // TODO: These variable names are ambiguous and ought to be updated in another
   // PR. The Calcsize references many of these variables.
-  Container<Real> dgnumlo;
-  Container<Real> dgnumhi;
-  Container<Real> dgnum;
-  Container<Real> alnsg;
+  view_1d_pack_type dgnumlo;
+  view_1d_pack_type dgnumhi;
+  view_1d_pack_type dgnum;
+  view_1d_pack_type alnsg;
+  view_1d_pack_type population_offsets;
+
+  integral_type max_aer;
+  integral_type num_populations;
+  integral_type naer;
+  integral_type num_modes;
 };
 
 }  // namespace haero
