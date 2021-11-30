f05222b216e76cca2521763209d0b5f359a4561a
Asher Mancinelli <ashermancinelli@gmail.com>
2021-11-29 17:58:32 -0700

    Stub out new process test

--- haero/processes/mam_calcsize_process.hpp
+++ haero/processes/mam_calcsize_process.hpp
@@ -40,6 +40,34 @@ class MAMCalcsizeProcess final
             const Prognostics &prognostics, const Atmosphere &atmosphere,
             const Diagnostics &diagnostics,
             const Tendencies &tendencies) const override{};
+
+private:
+  static constexpr int top_level = 0;
+  static constexpr bool do_adjust = true;
+  static constexpr bool do_aitacc_transfer = true;
+
+  int nmodes;
+  int max_nspec;
+  int num_populations;
+  int aitken_idx;
+  int accum_idx;
+
+  std::vector<int> population_offsets;
+  std::vector<int> num_mode_species;
+
+  // NOTE: this has been linearized just like the aero species/modes held in
+  // the modal_aerosol_config.
+  std::vector<int> spec_density;
+
+  std::vector<Real> v2nmin_nmodes;
+  std::vector<Real> v2nmax_nmodes;
+  std::vector<Real> dgnmin_nmodes;
+  std::vector<Real> dgnmax_nmodes;
+
+  // There is a common factor calculated over and over in the core loop of this
+  // process. This factor has been pulled out so the calculation only has to be
+  // performed once.
+  std::vector<Real> common_factor_nmodes;
 };
 
 }  // namespace haero
--- haero/tests/mam_calcsize_process_tests.cpp
+++ haero/tests/mam_calcsize_process_tests.cpp
@@ -3,12 +3,19 @@
 #include <memory>
 
 #include "catch2/catch.hpp"
-#include "haero/processes/mam_calcsize_hostcxx_process.hpp"
+#include "haero/model.hpp"
+#include "haero/processes/mam_calcsize_process.hpp"
 
 using namespace haero;
 
+Model* get_model_for_unit_tests(const ModalAerosolConfig& aero_config,
+                                const int num_levels) {
+  static Model* model(Model::ForUnitTests(aero_config, num_levels));
+  return model;
+}
+
 // ADD COMMENTS
-TEST_CASE("mam_calcsize_run", "") {
+TEST_CASE("mam_calcsize_hostcxx_run", "") {
   /*-----------------------------------------------------------------------------
     We construct a phony model and initialize only the parts which we need to
     drive the test. Some of the fields defined below are created to satisfy the
@@ -16,16 +23,28 @@ TEST_CASE("mam_calcsize_run", "") {
    -----------------------------------------------------------------------------*/
 
   auto aero_config =
-      ModalAerosolConfig::create_mam4_config();  // create MAM4 configuration
+      create_mam4_modal_aerosol_config();  // create MAM4 configuration
 
-  int num_gases = aero_config.num_gases();
-  int num_modes = aero_config.num_modes();
-  int num_aero_populations = aero_config.num_aerosol_populations;
-  int num_levels = 72;
-  int num_vert_packs = PackInfo::num_packs(num_levels);
-  int num_iface_packs = PackInfo::num_packs(num_levels + 1);
+  static constexpr int num_levels{72};  // number of levels
+
+  std::size_t num_vert_packs = num_levels / HAERO_PACK_SIZE;
+  if (num_vert_packs * HAERO_PACK_SIZE < num_levels) {
+    num_vert_packs++;
+  }
+  std::size_t num_iface_packs = (num_levels + 1) / HAERO_PACK_SIZE;
+  if (num_iface_packs * HAERO_PACK_SIZE < (num_levels + 1)) {
+    num_iface_packs++;
+  }
+
+  auto* model = get_model_for_unit_tests(
+      aero_config, num_levels);  // get an instance of "model"
+
+  const size_t num_gases = aero_config.gas_species.size();    // number of gases
+  const size_t num_modes = aero_config.aerosol_modes.size();  // number of modes
 
   // Set up some prognostics aerosol data views
+  const int num_aero_populations{
+      model->num_aerosol_populations()};  // total number of aerosol species
   Kokkos::View<PackType**> int_aerosols(
       "interstitial aerosols", num_aero_populations,
       num_vert_packs);  // interstitial aerosols mmr [kg/kg(of air)]
@@ -51,16 +70,16 @@ TEST_CASE("mam_calcsize_run", "") {
   Real pblh{100.0};  // planetary BL height [m]
   auto atm = new Atmosphere(num_levels, temp, press, qv, ht, pdel, pblh);
 
-  // This will drive the "run" method of calcsize
-  SECTION("calcsize_run") {
+  // This will drive the "run" method of calcsize_hostcxx
+  SECTION("calcsize_hostcxx_run") {
     auto process = new MAMCalcsizeProcess();
 
     // Initialize prognostic and diagnostic variables, and construct a
     // tendencies container.
-    auto progs =
-        new Prognostics(aero_config, num_levels, int_aerosols, cld_aerosols,
-                        int_num_mix_ratios, cld_num_mix_ratios, gases);
-    auto diags = new HostDiagnostics(aero_config, num_levels);
+    auto progs = model->create_prognostics(int_aerosols, cld_aerosols,
+                                           int_num_mix_ratios,
+                                           cld_num_mix_ratios, gases);
+    auto diags = model->create_diagnostics();
     auto tends = new Tendencies(*progs);
 
     // Define a pseudo-random generator [0-1) that is consistent across
@@ -113,6 +132,6 @@ TEST_CASE("mam_calcsize_run", "") {
     delete progs;
     delete diags;
     delete process;
-  }  // section:calcsize_run
+  }  // section:calcsize_hostcxx_run
 
-}  // TEST_CASE mam_calcsize_run
+}  // TEST_CASE mam_calcsize_hostcxx_run
