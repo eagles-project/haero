683f8af83e37598b13ca42f2e5c7ccfa3c1b6b3b
Michael J. Schmidt <mschmidt271@gmail.com>
2022-01-06 12:14:52 -0700

    emission test working

--- chem_driver/tests/chem_driver_tests.cpp
+++ chem_driver/tests/chem_driver_tests.cpp
@@ -14,7 +14,7 @@ TEST_CASE("TChem tendency computation tests", "haero_unit_tests") {
     std::string input_file = prefix;
     input_file += "/config_arrhenius.yaml";
     std::string refsol_file = prefix;
-    refsol_file += "/camp_arrhenius.yaml";
+    refsol_file += "/output_arrhenius.yaml";
 
     Real tbeg = 0.0;
     Real tend = 100.0;
@@ -80,7 +80,7 @@ TEST_CASE("TChem tendency computation tests", "haero_unit_tests") {
     std::string input_file = prefix;
     input_file += "/config_troe.yaml";
     std::string refsol_file = prefix;
-    refsol_file += "/camp_troe.yaml";
+    refsol_file += "/output_troe.yaml";
 
     Real tbeg = 0.0;
     Real tend = 100.0;
@@ -146,7 +146,7 @@ TEST_CASE("TChem tendency computation tests", "haero_unit_tests") {
     std::string input_file = prefix;
     input_file += "/config_photolysis.yaml";
     std::string refsol_file = prefix;
-    refsol_file += "/camp_photolysis.yaml";
+    refsol_file += "/output_photolysis.yaml";
 
     Real tbeg = 0.0;
     Real tend = 100.0;
@@ -212,7 +212,7 @@ TEST_CASE("TChem tendency computation tests", "haero_unit_tests") {
     std::string input_file = prefix;
     input_file += "/config_cmaq_h2o2.yaml";
     std::string refsol_file = prefix;
-    refsol_file += "/CMAQ_H2O2_output.yaml";
+    refsol_file += "/output_CMAQ_H2O2.yaml";
 
     Real tbeg = 0.0;
     Real tend = 100.0;
@@ -278,7 +278,7 @@ TEST_CASE("TChem tendency computation tests", "haero_unit_tests") {
     std::string input_file = prefix;
     input_file += "/config_cmaq_oh_hno3.yaml";
     std::string refsol_file = prefix;
-    refsol_file += "/CMAQ_OH_HNO3_output.yaml";
+    refsol_file += "/output_CMAQ_OH_HNO3.yaml";
 
     Real tbeg = 0.0;
     Real tend = 100.0;
@@ -335,6 +335,74 @@ TEST_CASE("TChem tendency computation tests", "haero_unit_tests") {
     tol = 1e-7;
     for (int i = 0; i < nspec; ++i)
     {
+      REQUIRE(FloatingPoint<Real>::zero(abs(hsolution(i, nsteps - 1) - refsol(i, nsteps - 1)), tol));
+    }
+  }
+
+  SECTION("simple emission") {
+    std::string prefix = HAERO_TEST_DATA_DIR;
+    std::string input_file = prefix;
+    input_file += "/config_emission.yaml";
+    std::string refsol_file = prefix;
+    refsol_file += "/output_emission.yaml";
+
+    Real tbeg = 0.0;
+    Real tend = 100.0;
+    Real dt = 1.0;
+    Real cur_time = tbeg;
+    ordinal_type nsteps = 101;
+    ordinal_type nspec = 2;
+
+    auto refsol = Real_2d_view_host("refsol", nspec, nsteps);
+    std::vector<std::string> specs = {"A", "B", "C"};
+
+    auto root = YAML::LoadFile(refsol_file);
+    for (int i = 0; i < nspec; ++i) {
+      auto node = root[specs[i]];
+      int j = 0;
+      for (auto iter : node) {
+        refsol(i, j) = iter.as<Real>();
+        ++j;
+      }
+    }
+
+    auto solution = Real_2d_view_host("solution", nspec, nsteps);
+    auto hsolution = Kokkos::create_mirror_view(solution);
+
+    ChemSolver chem_solver(input_file);
+
+    auto cur_sol = chem_solver.get_state();
+
+    Kokkos::parallel_for(
+        "copy_sol", nspec,
+        KOKKOS_LAMBDA(const int& i) { solution(i, 0) = cur_sol(0, i + 3); });
+
+    for (int i = 1; i < nsteps; ++i) {
+      chem_solver.time_integrate(cur_time, cur_time + dt);
+      cur_sol = chem_solver.get_state();
+      cur_time += dt;
+      Kokkos::parallel_for(
+          "copy_sol", nspec,
+          KOKKOS_LAMBDA(const int& j) { solution(j, i) = cur_sol(0, j + 3); });
+    }
+    Kokkos::deep_copy(hsolution, solution);
+
+    Real tol = 1e-10;
+    auto error = Real_1d_view_host("error", nspec);
+    for (int i = 0; i < nspec; ++i) {
+      error(i) = 0.0;
+      for (int j = 0; j < nsteps; ++j) {
+        error(i) += pow(hsolution(i, j) - refsol(i, j), 2);
+      }
+      error(i) = sqrt(error(i));
+      std::cout << "error(i) = " << error(i) << "\n";
+      REQUIRE(FloatingPoint<Real>::zero(error(i), tol));
+    }
+
+    tol = 1e-10;
+    for (int i = 0; i < nspec; ++i)
+    {
+      std::cout << "error = " << abs(hsolution(i, nsteps - 1) - refsol(i, nsteps - 1)) << "\n";
       REQUIRE(FloatingPoint<Real>::zero(abs(hsolution(i, nsteps - 1) - refsol(i, nsteps - 1)), tol));
     }
   }
