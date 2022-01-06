cd011b67146ad4d256c4f7bdf850e199f5fcafea
Michael J. Schmidt <mschmidt271@gmail.com>
2022-01-06 11:33:07 -0700

    cmaq_h202 test working

--- chem_driver/tests/chem_driver_tests.cpp
+++ chem_driver/tests/chem_driver_tests.cpp
@@ -206,4 +206,71 @@ TEST_CASE("TChem tendency computation tests", "haero_unit_tests") {
       REQUIRE(FloatingPoint<Real>::zero(abs(hsolution(i, nsteps - 1) - refsol(i, nsteps - 1)), tol));
     }
   }
+
+  SECTION("simple CMAQ_H2O2") {
+    std::string prefix = HAERO_TEST_DATA_DIR;
+    std::string input_file = prefix;
+    input_file += "/config_cmaq_h2o2.yaml";
+    std::string refsol_file = prefix;
+    refsol_file += "/CMAQ_H2O2_output.yaml";
+
+    Real tbeg = 0.0;
+    Real tend = 100.0;
+    Real dt = 1.0;
+    Real cur_time = tbeg;
+    ordinal_type nsteps = 101;
+    ordinal_type nspec = 3;
+
+    auto refsol = Real_2d_view_host("refsol", nspec, nsteps);
+    std::vector<std::string> specs = {"A", "B", "C"};
+
+    auto root = YAML::LoadFile(refsol_file);
+    for (int i = 0; i < nspec; ++i) {
+      auto node = root[specs[i]];
+      int j = 0;
+      for (auto iter : node) {
+        refsol(i, j) = iter.as<Real>();
+        ++j;
+      }
+    }
+
+    auto solution = Real_2d_view_host("solution", nspec, nsteps);
+    auto hsolution = Kokkos::create_mirror_view(solution);
+
+    ChemSolver chem_solver(input_file);
+
+    auto cur_sol = chem_solver.get_state();
+
+    Kokkos::parallel_for(
+        "copy_sol", nspec,
+        KOKKOS_LAMBDA(const int& i) { solution(i, 0) = cur_sol(0, i + 3); });
+
+    for (int i = 1; i < nsteps; ++i) {
+      chem_solver.time_integrate(cur_time, cur_time + dt);
+      cur_sol = chem_solver.get_state();
+      cur_time += dt;
+      Kokkos::parallel_for(
+          "copy_sol", nspec,
+          KOKKOS_LAMBDA(const int& j) { solution(j, i) = cur_sol(0, j + 3); });
+    }
+    Kokkos::deep_copy(hsolution, solution);
+
+    Real tol = 1e-4;
+    auto error = Real_1d_view_host("error", nspec);
+    for (int i = 0; i < nspec; ++i) {
+      error(i) = 0.0;
+      for (int j = 0; j < nsteps; ++j) {
+        error(i) += pow(hsolution(i, j) - refsol(i, j), 2);
+      }
+      error(i) = sqrt(error(i));
+      REQUIRE(FloatingPoint<Real>::zero(error(i), tol));
+    }
+
+    tol = 1e-7;
+    for (int i = 0; i < nspec; ++i)
+    {
+      std::cout << "error = " << abs(hsolution(i, nsteps - 1) - refsol(i, nsteps - 1)) << "\n";
+      REQUIRE(FloatingPoint<Real>::zero(abs(hsolution(i, nsteps - 1) - refsol(i, nsteps - 1)), tol));
+    }
+  }
 }
