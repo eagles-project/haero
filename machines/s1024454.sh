1a50acca9ab2d18939f11ecd7c3f6a7e5a3145e8
Peter Bosler <pabosle@sandia.gov>
2022-05-02 08:50:53 -0600

    updated machine file to new rhel8 rebuild

--- machines/s1024454.sh
+++ machines/s1024454.sh
@@ -1,6 +1,6 @@
 # This file sets up the environment variables for building MAM on s1024454.srn.sandia.gov
 #
-# It's a rhel7 linux box with an intel skylake gold (24 cores with 2 threads per core over 2 sockets)
+# It's a rhel8 linux box with an intel skylake gold (24 cores with 2 threads per core over 2 sockets)
 # with an nvidia maxwell gpu.
 #
 
@@ -9,12 +9,12 @@ export HDF5_LIBRARY_DIR=$HDF5_ROOT/lib
 export HDF5_LIBRARY=libhdf5.a
 export HDF5_HL_LIBRARY=libhdf5_hl.a
 
-export NETCDF_INCLUDE_DIR=$NETCDF_ROOT/include
-export NETCDF_LIBRARY_DIR=$NETCDF_ROOT/lib
+export NETCDF_INCLUDE_DIR=$NETCDF_C_ROOT/include
+export NETCDF_LIBRARY_DIR=$NETCDF_C_ROOT/lib
 export NETCDF_LIBRARY=libnetcdf.a
 
-export NETCDFF_INCLUDE_DIR=$NETCDF_ROOT/include
-export NETCDFF_LIBRARY_DIR=$NETCDF_ROOT/lib
+export NETCDFF_INCLUDE_DIR=$NETCDF_FORTRAN_ROOT/include
+export NETCDFF_LIBRARY_DIR=$NETCDF_FORTRAN_ROOT/lib
 export NETCDFF_LIBRARY=libnetcdff.a
 
 # export OPENBLAS_INCLUDE_DIR=/home/mjschm/OpenBLAS/install/include
