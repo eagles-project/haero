1c409bd646aaf803fb2c3c6683f531ee6481d9e2
Overfelt <jroverf@sandia.gov>
2022-07-15 10:41:16 -0600

    Some changes based on code review.

--- cmake/create_unit_test.cmake
+++ cmake/create_unit_test.cmake
@@ -10,6 +10,25 @@ function(CreateUnitTest test_name test_srcs)
         MPI_EXEC_NAME ${HAERO_MPI_EXEC}
         MPI_NP_FLAG ${HAERO_MPI_NP_FLAG}
         MPI_EXTRA_ARGS ${HAERO_MPI_EXTRA_ARGS}
+        LIBS ${HAERO_LIBRARIES})
+    else()
+      EkatCreateUnitTest(${test_name} ${test_srcs}
+        MPI_EXEC_NAME ${HAERO_MPI_EXEC}
+        MPI_NP_FLAG ${HAERO_MPI_NP_FLAG}
+        LIBS ${HAERO_LIBRARIES})
+    endif()
+  else()
+    EkatCreateUnitTest(${test_name} ${test_srcs} LIBS skywalker;${HAERO_LIBRARIES})
+  endif()
+endfunction()
+
+function(CreateSkywalkerTest test_name test_srcs)
+  if (HAERO_MPI_EXEC)
+    if (HAERO_MPI_EXTRA_ARGS)
+      EkatCreateUnitTest(${test_name} ${test_srcs}
+        MPI_EXEC_NAME ${HAERO_MPI_EXEC}
+        MPI_NP_FLAG ${HAERO_MPI_NP_FLAG}
+        MPI_EXTRA_ARGS ${HAERO_MPI_EXTRA_ARGS}
         LIBS skywalker;${HAERO_LIBRARIES})
     else()
       EkatCreateUnitTest(${test_name} ${test_srcs}
