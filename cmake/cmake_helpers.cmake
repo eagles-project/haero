c2e96ad8add7e0c7e816600035f3dfddc0ea70db
Jeffrey N. Johnson <jeff@cohere-llc.com>
2022-08-29 11:53:02 -0700

    Getting build config info into shape.

--- cmake/cmake_helpers.cmake
+++
@@ -1,8 +0,0 @@
-function (printvar var)
-    message("${var}: ${${var}}")
-endfunction()
-
-function (list2str list str)
-    string(REPLACE ";" " " tmp "${list}")
-    set(${str} ${tmp} PARENT_SCOPE)
-endfunction()
