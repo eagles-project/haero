09d01d5601c49cf497cb550eaddebe072797adf3
Jeffrey N. Johnson <jeff@cohere-llc.com>
2023-04-10 10:04:02 -0700

    Fixing an issue related to builds within mam4xx.

--- haero/atmosphere.cpp
+++ haero/atmosphere.cpp
@@ -14,14 +14,15 @@ static constexpr size_t num_views = 7;
 // this constructor allocates memory on-device for all of its views
 Atmosphere::Atmosphere(int num_levels, Real pblh)
     : num_levels_(num_levels),
-      view_storage_(reinterpret_cast<Real*>(Kokkos::kokkos_malloc(sizeof(Real) * num_views * num_levels))),
+      view_storage_(reinterpret_cast<Real *>(
+          Kokkos::kokkos_malloc(sizeof(Real) * num_views * num_levels))),
       temperature(&view_storage_[0], num_levels),
       pressure(&view_storage_[num_levels], num_levels),
-      vapor_mixing_ratio(&view_storage_[2*num_levels], num_levels),
-      height(&view_storage_[3*num_levels], num_levels),
-      hydrostatic_dp(&view_storage_[4*num_levels], num_levels),
-      cloud_fraction(&view_storage_[5*num_levels], num_levels),
-      updraft_vel_ice_nucleation(&view_storage_[6*num_levels], num_levels),
+      vapor_mixing_ratio(&view_storage_[2 * num_levels], num_levels),
+      height(&view_storage_[3 * num_levels], num_levels),
+      hydrostatic_dp(&view_storage_[4 * num_levels], num_levels),
+      cloud_fraction(&view_storage_[5 * num_levels], num_levels),
+      updraft_vel_ice_nucleation(&view_storage_[6 * num_levels], num_levels),
       planetary_boundary_height(pblh) {
   EKAT_REQUIRE_MSG(num_levels > 0,
                    "Number of vertical levels must be positive");
@@ -34,10 +35,10 @@ Atmosphere::Atmosphere(int num_levels, const ColumnView temp,
                        const ColumnView ht, const ColumnView pdel,
                        const ColumnView cloud_f, const ColumnView uv_ice_nuc,
                        Real pblh)
-    : num_levels_(num_levels), view_storage_(nullptr),
-      temperature(temp), pressure(press), vapor_mixing_ratio(qv), height(ht),
-      hydrostatic_dp(pdel), cloud_fraction(cloud_f),
-      updraft_vel_ice_nucleation(uv_ice_nuc), planetary_boundary_height(pblh) {
+    : num_levels_(num_levels), view_storage_(nullptr), temperature(temp),
+      pressure(press), vapor_mixing_ratio(qv), height(ht), hydrostatic_dp(pdel),
+      cloud_fraction(cloud_f), updraft_vel_ice_nucleation(uv_ice_nuc),
+      planetary_boundary_height(pblh) {
   EKAT_REQUIRE_MSG(num_levels > 0,
                    "Number of vertical levels must be positive");
   EKAT_REQUIRE_MSG(pblh >= 0.0,
--- haero/testing.cpp
+++ haero/testing.cpp
@@ -2,7 +2,7 @@
 // LLC (NTESS). Copyright (c) 2022, Battelle Memorial Institute
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include <haero/testing.hpp>
+#include "testing.hpp"
 
 namespace haero {
 
@@ -14,27 +14,24 @@ using Real = haero::Real;
 // (e.g.) unit tests. A ColumnPool manages a number of ColumnViews with a fixed
 // number of vertical levels.
 class ColumnPool {
-  size_t              num_levels_;  // number of vertical levels per column (fixed)
-  size_t              num_cols_;    // number of allocated columns
+  size_t num_levels_; // number of vertical levels per column (fixed)
+  size_t num_cols_;   // number of allocated columns
   std::vector<size_t> col_offsets_; // offsets of columns in memory_
   std::vector<size_t> col_in_use_;  // an array indicating whether a column is
                                     // being used
 
-  Real               *memory_;      // memory pool itself (allocated on device)
-  size_t              size_;        // size of memory pool in bytes
- public:
-
+  Real *memory_; // memory pool itself (allocated on device)
+  size_t size_;  // size of memory pool in bytes
+public:
   // constructs a column pool with the given initial number of columns, each
   // with the given number of vertical levels.`
   ColumnPool(size_t num_vertical_levels, size_t initial_num_columns = 64)
-    : num_levels_(num_vertical_levels),
-      num_cols_(initial_num_columns),
-      col_in_use_(initial_num_columns, 0),
-      memory_(reinterpret_cast<Real*>(
-        Kokkos::kokkos_malloc("Column pool",
-                              sizeof(Real) * num_vertical_levels * initial_num_columns))),
-      size_(sizeof(Real) * num_vertical_levels * initial_num_columns) {
-  }
+      : num_levels_(num_vertical_levels), num_cols_(initial_num_columns),
+        col_in_use_(initial_num_columns, 0),
+        memory_(reinterpret_cast<Real *>(Kokkos::kokkos_malloc(
+            "Column pool",
+            sizeof(Real) * num_vertical_levels * initial_num_columns))),
+        size_(sizeof(Real) * num_vertical_levels * initial_num_columns) {}
 
   // no copying of the pool
 
@@ -46,13 +43,15 @@ class ColumnPool {
     // find the first unused column
     size_t i;
     for (i = 0; i < num_cols_; ++i) {
-      if (!col_in_use_[i]) break;
+      if (!col_in_use_[i])
+        break;
     }
     if (i == num_cols_) { // all columns in the pool are in use!
       // double the number of allocated columns in the pool
       num_cols_ *= 2;
       col_in_use_.resize(num_cols_, 0);
-      memory_ = reinterpret_cast<Real*>(Kokkos::kokkos_realloc(memory_, sizeof(Real) * num_levels_ * num_cols_));
+      memory_ = reinterpret_cast<Real *>(Kokkos::kokkos_realloc(
+          memory_, sizeof(Real) * num_levels_ * num_cols_));
     }
 
     col_in_use_[i] = true;
@@ -64,7 +63,7 @@ class ColumnPool {
 // column pools, organized by column resolution
 std::map<size_t, std::unique_ptr<ColumnPool>> pools_{};
 
-}
+} // namespace
 
 namespace testing {
 
@@ -72,14 +71,13 @@ ColumnView create_column_view(int num_levels) {
   // find a column pool for the given number of vertical levels
   auto iter = pools_.find(num_levels);
   if (iter == pools_.end()) {
-    auto result = pools_.emplace(num_levels,
-                                 std::unique_ptr<ColumnPool>(new ColumnPool(num_levels)));
+    auto result = pools_.emplace(
+        num_levels, std::unique_ptr<ColumnPool>(new ColumnPool(num_levels)));
     iter = result.first;
   }
   return iter->second->column_view();
 }
 
-} // testing
+} // namespace testing
 
-} // haero
-
+} // namespace haero
