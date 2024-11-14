2bcf26d970ba2fd3e7b2916a3f6289b249ace34a
Jaelyn Litzinger <jaelyn.litzinger@pnnl.gov>
2024-11-13 16:18:49 -0800

    use cuda12.3 and gcc12.2.0

--- machines/deception.sh
+++ machines/deception.sh
@@ -2,7 +2,7 @@
 . /etc/profile.d/modules.sh
 module purge
 
-module load gcc/11.2.0
+module load gcc/12.2.0
 module load cmake/3.28.1
 module load python/3.7.0
-module load cuda/11.7
+module load cuda/12.3
