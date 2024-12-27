8e730c43ebabce54445356e47a6e26a9afbb90a3
Jaelyn Litzinger <93339979+jaelynlitz@users.noreply.github.com>
2024-12-27 12:48:23 -0800

    Upgrade modules for Deception (#485)
    * change gcc and cuda version
    
    * use cuda12.3 and gcc12.2.0

--- machines/deception.sh
+++ machines/deception.sh
@@ -2,7 +2,7 @@
 . /etc/profile.d/modules.sh
 module purge
 
-module load gcc/10.2.0
+module load gcc/12.2.0
 module load cmake/3.28.1
 module load python/3.7.0
-module load cuda/11.4
+module load cuda/12.3
